26f16047f6d175532fddf3c91689dff2e4cf9b47
Alessandro Menezes <alessandroadm@live.com>
2020-03-27 21:20:11 -0300

    pagina home

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* Rota para renderizar a pagina inicial do site */
+router.get('/home', (req, res, next) => {
+  res.render('home');
+});
+
 module.exports = router;
