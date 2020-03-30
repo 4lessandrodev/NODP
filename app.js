3b0c3b09c8a773af0f20289e001939d811430fad
Alessandro Menezes <alessandroadm@live.com>
2020-03-30 01:55:38 -0300

    criado o componente de perfil

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -18,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+ 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('entrar', { title: 'Login' });
+});
+
+
+/* GET home page. */
+router.get('/index', function (req, res, next) {
+  res.render('index', { title: 'Login' });
 });
 
 
