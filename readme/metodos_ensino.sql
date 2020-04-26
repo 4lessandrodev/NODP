a3170a8b4394f9a521e79d7724bed66048b928c7
Alessandro Menezes <alessandroadm@live.com>
2020-04-25 23:41:15 -0300

    atualizando inserts

--- readme/metodos_ensino.sql
+++
@@ -1,4 +0,0 @@
-USE NODP_BD;
-INSERT INTO metodos_ensino (descricao) VALUES('online'),
-('presencial'),('online/presencial');
-SELECT * FROM metodos_ensino
--- readme/tipos_notificacoes.sql
+++
@@ -1,4 +0,0 @@
-Use nodp_bd;
-
-INSERT INTO tipos_notificacoes (descricao) VALUES ('Enviou mensagem'), ('Deu medalha na sua postagem'),
-('Deu moeda na sua postagem'), ('Apoiou você'), ('Comentou na sua postagem');
