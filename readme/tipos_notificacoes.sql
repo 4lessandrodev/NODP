982650c61e22ea830265d0329d0b026c78174cb9
André Oliveira <andreluisoliveira013@gmail.com>
2020-04-25 21:36:09 -0300

    Atualizando scripts sql do projeto

---
+++ readme/tipos_notificacoes.sql
@@ -0,0 +1,4 @@
+Use nodp_bd;
+
+INSERT INTO tipos_notificacoes (descricao) VALUES ('Enviou mensagem'), ('Deu medalha na sua postagem'),
+('Deu moeda na sua postagem'), ('Apoiou você'), ('Comentou na sua postagem');
